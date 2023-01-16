32bf08caaad0f942f9dd3a3ce3595d2a5bef339b
Ryan <ryan.xue2@gmail.com>
2023-01-16 11:19:24 -0500

    some movement logic

--- src/main.py
+++ src/main.py
@@ -1,13 +1,36 @@
 import pgzrun
 from main_character import MainCharacter
 
-main_char = MainCharacter()
+
+WIDTH = 800
+HEIGHT = 500
+
+main_char = MainCharacter(WIDTH, HEIGHT)
 
 def draw():
     screen.clear()
     main_char.blit()
 
+
 def update():
-    main_char.actor.x += 1
+    main_char.update_position()
+
+
+def on_key_down(key):
+    if key == keys.RIGHT or key == keys.D:
+        main_char.moving_right = True
+    elif key == keys.LEFT or key == keys.A:
+        main_char.moving_left = True
+    elif key == keys.UP or key == keys.W:
+        main_char.jumping = True
+
+def on_key_up(key):
+    if key == keys.RIGHT or key == keys.D:
+        main_char.moving_right = False
+    elif key == keys.LEFT or key == keys.A:
+        main_char.moving_left = False
+    elif key == keys.UP or key == keys.W:
+        main_char.jumping = False
+
 
 pgzrun.go()
--- src/main_character.py
+++ src/main_character.py
@@ -1,16 +1,31 @@
 from pgzero.builtins import Actor, Rect
 
+
 class MainCharacter:
-    def __init__(self):
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+
+        self.speed = 5
+        self.jump_force = 10
+        self.gravity = 1
+
+        self.moving_left = False
+        self.moving_right = False
+        self.jumping = False
+        
         self.x = 0
         self.y = 0
-        self.sword_x = 0
-        self.sword_y = 0
+
+        self.y_velocity = 0
+
         self.actor = Actor('main_character')
         self.sword_actor = Actor('mythicalsword')
+
         print(dir(Actor))
         print(Actor.image)
         print(self.actor.pos)
+
         self.rect = Rect(self.actor.x+50, self.actor.y, 10, 10)
 
 
@@ -18,6 +33,37 @@ class MainCharacter:
         if rect.colliderect(self.rect):
             return True
         return False
+    
+    def update_position(self):
+        if self.jumping and not self._is_midair():
+            self.y_velocity -= self.jump_force
+        
+        if self._is_midair():
+            self.y_velocity += self.gravity
+
+        self.y += self.y_velocity
+
+        if self.moving_right ^ self.moving_left:
+            if self.moving_left:
+                self.x -= self.speed
+            elif self.moving_right:
+                self.x += self.speed
+
+        if self.y + 100 >= self.screen_height:
+            self.y = self.screen_height - 100
+            self.y_velocity = 0
+
+        self.actor.x = self.x
+        self.actor.y = self.y
+        print(self.x, self.y)
+        # check if x is colliding with anything
+        # and put it back
+
+    
+    def _is_midair(self):
+        if self.y + 100 >= self.screen_height:
+            return False
+        return True
 
     def blit(self):
         self.actor.draw()
