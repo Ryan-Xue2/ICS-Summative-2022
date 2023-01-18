d15fe97b57b0db70948f480750e63ad7efb5f155
Ryan-Xue2 <ryan.xue2@gmail.com>
2023-01-17 23:11:15 -0500

    stuff

--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ import pgzrun
 from player import Player
 from settings import screen_width, screen_height
 
+
 with open('levels.json') as levels_file:
     levels = json.load(levels_file)
+    block_ids = levels['block_ids']
+    levels = levels['level_maps']
+
+print(block_ids)
 
 WIDTH = screen_width
 HEIGHT = screen_height
@@ -17,6 +22,7 @@ bg_img = Actor('background')
 def draw():
     """Draw the background image, the level, and the player to the screen"""
     bg_img.draw()
+    draw_level(levels[0])
     player.blit(screen)
 
 
@@ -44,8 +50,12 @@ def on_key_up(key):
         player.jumping = False
 
 
-def draw_level(level):
-    pass
+def draw_level(level_map):
+    block_width, block_height = 50, 50
+    for i, row in enumerate(level_map):
+        for j, block in enumerate(row):
+            if block_ids[str(block)]['type'] == 'solid':
+                screen.draw.filled_rect(Rect(j*50, i*50, 50, 50), 'black')
 
 
 def load_next_level():
